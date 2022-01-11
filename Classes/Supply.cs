73e4d84ac9b53121e0d44c24593569f0770935f7
Connor12858 <connor.godden2006@gmail.com>
2022-01-11 16:37:51 -0500

    f

--- Classes/Supply.cs
+++ Classes/Supply.cs
@@ -14,6 +14,8 @@ namespace TeamManager.Classes
         private int _quantity;
         public int CaseOf { get { return _caseOf; } }
         private int _caseOf;
+        public int RemainderOfCase { get { return _remainderOfCase; } }
+        private int _remainderOfCase;
         public int TotalAmount { get { return _totalAmount; } }
         private int _totalAmount;
 
@@ -29,6 +31,46 @@ namespace TeamManager.Classes
             _totalAmount = q * c;
         }
 
-        public void //Edit amounts
+        public void SupplyDropOff(int packages)
+        {
+            _quantity += packages;
+            _totalAmount = _quantity * _caseOf;
+        }
+
+        public void DeliveredSupply(int amount)
+        {
+            if (_remainderOfCase == 0)
+            {
+                if (amount % _caseOf == 0)
+                {
+                    _quantity -= amount / _caseOf;
+                    _totalAmount = (_quantity * _caseOf) + _remainderOfCase;
+                }
+                else
+                {
+                    _remainderOfCase = amount % _caseOf;
+                    amount -= _remainderOfCase;
+                    _quantity -= amount / _caseOf;
+                    _totalAmount = (_quantity * _caseOf) + _remainderOfCase;
+                }
+            }
+            else
+            {
+                amount -= _remainderOfCase;
+                _remainderOfCase = 0;
+                if (amount % _caseOf == 0)
+                {
+                    _quantity -= amount / _caseOf;
+                    _totalAmount = (_quantity * _caseOf) + _remainderOfCase;
+                }
+                else
+                {
+                    _remainderOfCase = amount % _caseOf;
+                    amount -= _remainderOfCase;
+                    _quantity -= amount / _caseOf;
+                    _totalAmount = (_quantity * _caseOf) + _remainderOfCase;
+                }
+            }
+        }
     }
 }
